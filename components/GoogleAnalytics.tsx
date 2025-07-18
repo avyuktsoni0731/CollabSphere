370653ba919a39bc5f8051f77775f9a8eb7e2aab
Avyukt Soni <soniavyukt@gmail.com>
2025-07-18 22:42:17 +0530

    Fixed Google Analytics on routes

--- components/GoogleAnalytics.tsx
+++ components/GoogleAnalytics.tsx
@@ -3,15 +3,21 @@
 import { usePathname } from "next/navigation";
 import { useEffect } from "react";
 
+declare global {
+  interface Window {
+    gtag: (...args: any[]) => void;
+  }
+}
+
 export const GoogleAnalytics = () => {
   const pathname = usePathname();
 
   useEffect(() => {
-    // @ts-ignore
     if (typeof window !== "undefined" && window.gtag) {
-      // @ts-ignore
-      window.gtag("config", process.env.NEXT_PUBLIC_G_ANALYTICS_ID as string, {
+      window.gtag("event", "page_view", {
         page_path: pathname,
+        page_location: window.location.href,
+        page_title: document.title,
       });
     }
   }, [pathname]);
